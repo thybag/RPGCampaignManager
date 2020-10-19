d242d54aec727011a5955830d7f64cc4272e1805
Vladislav Ogir <68771766+Spiker1992@users.noreply.github.com>
2020-10-19 23:03:15 +0100

    Updated campaign controller

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Campaign;
 use Auth;
+use App\Models\Campaign;
+use Illuminate\Http\Request;
 use App\Http\Requests\CampaignRequest;
 
 class CampaignController extends Controller
